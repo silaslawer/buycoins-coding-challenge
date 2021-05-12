71c2690ee3296973a1fb1b2ef3785a05bf69bca9
silaslawer <slawer31@gmail.com>
2021-05-12 18:12:23 +0000

    implement calculate_buy_price

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,10 +1,14 @@
-class Types::QueryType < Types::BaseObject
-  field :calculate_price, Float, description: "Returns calculated price", null: true do
-    argument :type, String, required: true
-    argument :margin, Float, required: true
-    argument :exchange_rate, Float, required: true
-  end
-  def calculate_price(type:, margin:, exchange_rate:)
-    20.30
+module Types
+  class QueryType < Types::BaseObject
+    field :calculate_price, Float, description: "Returns calculated price", null: true do
+      argument :type, String, required: true
+      argument :margin, Float, required: true
+      argument :exchange_rate, Float, required: true
+    end
+    def calculate_price(type:, margin:, exchange_rate:)
+      btc_price = Coindesk.bitcoin_price(:usd)
+
+      Coindesk.calculate_buy_price(btc_price: btc_price, margin: margin, exchange_rate: exchange_rate)
+    end
   end
 end
--- app/helpers/coindesk.rb
+++ app/helpers/coindesk.rb
@@ -6,7 +6,7 @@ class Coindesk
     JSON.parse(response)["bpi"][currency]["rate_float"]
   end
 
-  def calculate_buy_price(btc_price:, margin:, exchange_rate:)
+  def self.calculate_buy_price(btc_price:, margin:, exchange_rate:)
     margin = (btc_price * margin) / 100
     price = btc_price + margin
     price * exchange_rate
