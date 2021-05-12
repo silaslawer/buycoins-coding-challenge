6e5c72dca051373eac4497e8e944a030b0a9a818
silaslawer <slawer31@gmail.com>
2021-05-12 22:49:47 +0000

    reuse httparty

--- spec/helpers/coindesk_spec.rb
+++ spec/helpers/coindesk_spec.rb
@@ -5,12 +5,11 @@ require "net/http"
 describe "coindesk" do
 
   it "should return bitcoin price" do
-    hash = { "time" => { "updated": "Sep 18, 2013 17:27:00 UTC", "updatedISO": "2013-09-18T17:27:00+00:00" }, "disclaimer" => "This data was produced from the CoinDesk Bitcoin Price Index. Non-USD currency data converted using hourly conversion rate from openexchangerates.org", "bpi" => { "USD" => { "code": "USD", "symbol": "$", "rate": "126.5235", "description": "United States Dollar", "rate_float": 126.5235 }, "GBP" => { "code": "GBP", "symbol": "£", "rate": "79.2495", "description": "British Pound Sterling", "rate_float": 79.2495 }, "EUR" => { "code": "EUR", "symbol": "€", "rate": "94.7398", "description": "Euro", "rate_float": 94.7398 } } }
+    hash = '{ "time": { "updated": "Sep 18, 2013 17:27:00 UTC", "updatedISO": "2013-09-18T17:27:00+00:00" }, "disclaimer": "This data was produced from the CoinDesk Bitcoin Price Index. Non-USD currency data converted using hourly conversion rate from openexchangerates.org", "bpi": { "USD": { "code": "USD", "symbol": "$", "rate": "126.5235", "description": "United States Dollar", "rate_float": 126.5235 }, "GBP": { "code": "GBP", "symbol": "£", "rate": "79.2495", "description": "British Pound Sterling", "rate_float": 79.2495 }, "EUR": { "code": "EUR", "symbol": "€", "rate": "94.7398", "description": "Euro", "rate_float": 94.7398 } } }'
+    allow(Coindesk).to receive(:get).and_return(hash)
 
-    allow(Coindesk).to receive(:bitcoin_price).with(:usd).and_return(hash)
-
-    res = Coindesk.bitcoin_price(:usd)
-    expect(res["bpi"]["USD"][:rate_float]).to eq(126.5235)
+    res = Coindesk.bitcoin_price :usd
+    expect(res).to eq(126.5235)
   end
 
   it "calculates buying price" do
