049aa193bc8860819ea8f801f9f4bea9b05b7b19
silaslawer <slawer31@gmail.com>
2021-05-12 18:09:40 +0000

    calculate_buy_price function

--- app/helpers/coindesk.rb
+++ app/helpers/coindesk.rb
@@ -5,4 +5,10 @@ class Coindesk
     response = Net::HTTP.get(URI.parse("https://api.coindesk.com/v1/bpi/currentprice/#{currency}.json"))
     JSON.parse(response)["bpi"][currency]["rate_float"]
   end
+
+  def calculate_buy_price(btc_price:, margin:, exchange_rate:)
+    margin = (btc_price * margin) / 100
+    price = btc_price + margin
+    price * exchange_rate
+  end
 end
