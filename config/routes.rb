c5d27ffd1434f49f7aea1a11bd33c435afc2059f
silaslawer <slawer31@gmail.com>
2021-05-13 02:22:04 +0000

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  if Rails.env.development?
-    mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
-  else
-    get "/", to: "graphql#heartbeat"
-  end
+  # if Rails.env.development?
+  #   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "graphql#execute"
+  # else
+  #   get "/", to: "graphql#heartbeat"
+  # end
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
   post "/graphql", to: "graphql#execute"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
